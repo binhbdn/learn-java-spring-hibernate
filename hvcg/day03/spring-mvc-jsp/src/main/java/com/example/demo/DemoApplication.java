998755134644a23c6ea064f82c862d872e67a264
binhbdn <binh.buidn@gmail.com>
2021-08-15 23:25:16 +0700

    return viewResolver

--- hvcg/day03/spring-mvc-jsp/src/main/java/com/example/demo/DemoApplication.java
+++ hvcg/day03/spring-mvc-jsp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,16 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Bean
+	public InternalResourceViewResolver viewResolver(){
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setApplicationContext(applicationContext);
+		viewResolver.setPrefix("/WEB-INF/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
 }
