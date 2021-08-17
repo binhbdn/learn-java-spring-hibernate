d31e834bc66d118784d5a5e694ce96686b6eef31
binhbdn <binh.buidn@gmail.com>
2021-08-17 18:17:48 +0700

    Add controller: retrieving students

---
+++ hvcg/day04/hibernate1/src/main/java/com/example/hibernate1/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.hibernate1.controller;
+
+import com.example.hibernate1.dao.SessionFactorySingleton;
+import com.example.hibernate1.model.Student;
+import org.hibernate.Session;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+    @GetMapping(value = "/student")
+    public List<Student> fetchStudents(){
+        Session session = SessionFactorySingleton.get().openSession();
+        //session.beginTransaction();
+        List<Student> results = (List<Student>) session.createQuery( "from Student" ).list(); // HQL - Hibernate Query Language
+        for ( Student student : (List<Student>) results ) {
+            System.out.println( "Student: " + student.toString());
+        }
+        //session.getTransaction().commit();
+        session.close();
+
+        return results;
+    }
+}
