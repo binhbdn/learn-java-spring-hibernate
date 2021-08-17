f02c064eb50411f5265134d526c19704d202fcfc
binhbdn <binh.buidn@gmail.com>
2021-08-17 11:56:46 +0700

    HomeWork: Spring+Thymeleaf+MySql

---
+++ hvcg/day03/spring-thymeleaf/src/main/java/vn/hvcg/thymeleafdemo/StudentController.java
@@ -0,0 +1,49 @@
+package vn.hvcg.thymeleafdemo;
+
+import org.springframework.stereotype.Controller;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class StudentController {
+    private static final List<Student> students = new ArrayList<>();
+
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String studentList(Model model) {
+        Connection con = null;
+        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/students", "root", "");
+            Statement stat = con.createStatement();
+            String query = "SELECT * FROM tbl_students";
+            ResultSet resultset = stat.executeQuery(query);
+
+            while (resultset.next()) {
+                int id = resultset.getInt("id");
+                String name = resultset.getString("name");
+                students.add(new Student(id, name));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        }
+
+        model.addAttribute("students", students);
+
+        return "students";
+    }
+
+}
