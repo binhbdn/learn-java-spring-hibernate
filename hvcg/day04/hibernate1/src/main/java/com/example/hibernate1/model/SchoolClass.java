e527f3c9d2f001b692033079ae79ac89e809e71a
binhbdn <binh.buidn@gmail.com>
2021-08-18 09:03:12 +0700

    SchoolClass: model

---
+++ hvcg/day04/hibernate1/src/main/java/com/example/hibernate1/model/SchoolClass.java
@@ -0,0 +1,33 @@
+package com.example.hibernate1.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "school_class")
+public class SchoolClass {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public SchoolClass() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
